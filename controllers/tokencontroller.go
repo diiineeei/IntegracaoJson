11341a8a7a7b45bf944b77fc7a846fd8f120be58
rodineii <rodinei.lima@track.co>
2023-05-28 15:48:03 -0300

    Adicionada geração de token jwt

---
+++ controllers/tokencontroller.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"github.com/diiineeei/IntegracaoJson/authjwt"
+	"github.com/diiineeei/IntegracaoJson/database"
+	"github.com/diiineeei/IntegracaoJson/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TokenRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func GenerateToken(context *gin.Context) {
+	var request TokenRequest
+	var user models.User
+	if err := context.ShouldBindJSON(&request); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+
+	// check if email exists
+	record := database.Instance.Where("email = ?", request.Email).First(&user)
+	if record.Error != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": record.Error.Error()})
+		context.Abort()
+		return
+	}
+
+	// check password is correct
+	credentialError := user.CheckPassword(request.Password)
+	if credentialError != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+		context.Abort()
+		return
+	}
+
+	tokenString, err := authjwt.GenerateJWT(user.Email, user.Name)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
